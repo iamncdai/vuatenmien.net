5995bae0c922b81e4f1b05c57fa5b40f1260d7f4
Nguyễn Chánh Đại <ncdai@penphy.edu.vn>
2023-09-27 00:57:58 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3004
+
+app.use(express.static('static'))
+app.set("view engine", "ejs")
+
+const DOMAINS = [
+  "DacSac.com",
+  "DuongSach.com",
+  "KietTac.com",
+  "KimCuongGroup.com",
+  "MayTinhViet.com",
+  "MetaBox.vn",
+  "NangLuongGio.com",
+  "San24h.com",
+  "SieuUngDung.com",
+  "TienDiDong.com",
+  "TienPhongGroup.com",
+  "Use.com.vn",
+  "VieBase.com",
+  "VieBus.com",
+  "VieCons.com",
+  "VieSafe.com",
+  "VuaDiDong.com",
+  "VuaNhaDat.com",
+  "VuongQuoc.net",
+  "VuongQuocHoa.com",
+];
+
+app.get('/', (req, res) => {
+  res.render("index", {
+    domains: DOMAINS
+  })
+})
+
+app.listen(port, () => {
+  console.log(`VuaTenMien.net app listening on port ${port}`)
+})
